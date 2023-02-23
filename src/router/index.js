80a35dba41f24d1e9af00e1e48d944a33757c333
guanwenxin <764434043@qq.com>
2023-02-23 18:00:32 +0800

    feat:登录页面+路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
+import HomeViewVue from '@/views/HomeView.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,15 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeViewVue
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/main',
+    name: 'main',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: MainView
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 0.v-bind -->
+  <div class="page">
+    <div class="bg-img">
+      <img src="../assets/login-background.png">
+    </div>
+    <div class="container">
+      <!-- <ChildComp></ChildComp> -->
+      <div class="desc"></div>
+      <div class="login">
+        <div class="login-wrapper">
+          <el-form :model="form" label-width="80px">
+            <div class="form-name">欢迎使用企业进销存管理系统</div>
+            <el-form-item label="用户名：">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码：">
+              <el-input v-model="form.passwd" show-password></el-input>
+            </el-form-item>
+          </el-form>
+          <div class="operator">
+            <el-button size="small" @click="login">登录</el-button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
+  // 预定义属性
   name: 'HomeView',
-  components: {
-    HelloWorld
+  // 组件当中所有的响应式数据
+  data: function () {
+    return {
+      form: {
+        name: '',
+        passwd: '',
+      },
+      isShowChild: true,
+      pInfo: '这是用来设置默认的input框的值',
+    }
+  },
+  // 方法
+  methods: {
+      login() {
+       // TODO: 跳转至主页面
+       this.$router.push({path:'main'})
+    },
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+/**
+使用了scoped关键字，当前样式只应用于此组件以及子组件，不会影响父组件
+*/
+.container {
+  display: flex;
+}
+.operator {
+  display: flex;
+  justify-content: center;
+}
+.desc {
+  width: 60%;
+}
+.login {
+  padding-top: 150px;
+  .login-wrapper {
+    background-color: #fff;
+    width: 300px;
+    padding: 40px;
+    border-radius: 10px;
+    .form-name{
+      font-size: larger;
+      margin: 15px 25px;
+    }
+  }
+}
+.bg-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+    height: 100%;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
