c2a0c685fdd7fbd956a9649bcaddba4a5ab23103
guanwenxin <764434043@qq.com>
2023-03-01 19:30:14 +0800

    feat:完成各个页面的返回顶部按钮

--- src/components/navbar/tjbbfx/khtjTab.vue
+++ src/components/navbar/tjbbfx/khtjTab.vue
@@ -1,11 +1,55 @@
+
+
 <template>
-    <div>
-       客户统计
+    <div class="default" ref="default" @scroll="setButton($event)">
+        <div ref="top">客户统计</div>
+        <div style="height: 20000px; background-color: pink;"></div>
+        <div v-show="needTopBtn" class="back"><el-button type="primary" @click="returnTop">回顶部</el-button></div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'khtjTab'
+    name:'khtjTab',
+
+    data: function () {
+        return {
+            needTopBtn: false,
+    }
+},
+    mounted(){
+
+    },
+
+    methods: {
+        returnTop() {
+            this.$refs.top.scrollIntoView(true, {
+                behavior: 'smooth'
+            });
+        },
+        setButton(e) {
+            if (e.target.scrollTop > 0) {
+                this.needTopBtn = true
+            } else {
+                this.needTopBtn = false
+            }
+        }
+    },
 }
+
+
 </script>
+
+<style lang="scss" scoped>
+.back {
+    position: fixed;
+    right: 40px;
+    bottom: 40px;
+}
+
+.default {
+    height: 100%;
+    overflow-y: auto;
+    padding: 0 20px;
+}
+</style>
--- src/components/navbar/xtgl/jsglTab.vue
+++ src/components/navbar/xtgl/jsglTab.vue
@@ -1,11 +1,54 @@
+
 <template>
-    <div>
-       角色管理
+    <div class="default" ref="default" @scroll="setButton($event)">
+        <div ref="top">角色管理</div>
+        <div style="height: 20000px; background-color: pink;"></div>
+        <div v-show="needTopBtn" class="back"><el-button type="primary" @click="returnTop">回顶部</el-button></div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'jsglTab'
+    name:'jsglTab',
+
+    data: function () {
+        return {
+            needTopBtn: false,
+    }
+},
+    mounted(){
+
+    },
+
+    methods: {
+        returnTop() {
+            this.$refs.top.scrollIntoView(true, {
+                behavior: 'smooth'
+            });
+        },
+        setButton(e) {
+            if (e.target.scrollTop > 0) {
+                this.needTopBtn = true
+            } else {
+                this.needTopBtn = false
+            }
+        }
+    },
 }
+
+
 </script>
+
+<style lang="scss" scoped>
+.back {
+    position: fixed;
+    right: 40px;
+    bottom: 40px;
+}
+
+.default {
+    height: 100%;
+    overflow-y: auto;
+    padding: 0 20px;
+}
+</style>
