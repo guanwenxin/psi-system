b6c7f76030828b69a09004ad609abc36704dfb20
guanwenxin <764434043@qq.com>
2023-03-14 09:54:13 +0800

    feat:完成用户添加功能

--- src/components/navbar/xtgl/yhglTab.vue
+++ src/components/navbar/xtgl/yhglTab.vue
@@ -2,18 +2,38 @@
 <template>
     <div class="default" ref="default" @scroll="setButton($event)">
         <div ref="top">用户管理</div>
-        <div style="height: 20000px; background-color: pink;"></div>
+        <!-- <div style="height: 20000px; background-color: pink;"></div> -->
         <div v-show="needTopBtn" class="back"><el-button type="primary" @click="returnTop">回顶部</el-button></div>
+
+
+        <el-button @click="sumbit">提交</el-button>
+        <el-form :model="form" label-width="80px">
+            <el-form-item label="用户名">
+                <el-input v-model="form.username"></el-input>
+            </el-form-item>
+            <el-form-item label="电话">
+                <el-input v-model="form.phone"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="form.password"></el-input>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
 <script>
+import { login } from '@/utils/login';
 export default {
     name:'yhglTab',
 
     data: function () {
         return {
             needTopBtn: false,
+            form: {
+                username: '',
+                password: '',
+                phone: ''
+            }
     }
 },
     mounted(){
@@ -32,6 +52,12 @@ export default {
             } else {
                 this.needTopBtn = false
             }
+        },
+
+
+        async sumbit() {
+            const result = await login.post('/user/add', this.form);
+            console.log('user', result)
         }
     },
 }
